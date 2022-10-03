c57eb071c95dc0ffae82adbc63eb5feb3697c8f9
Caio Uwarow de Souza <caio.souza@t-systems.com>
2022-10-03 10:01:08 -0300

    Initial Commit

---
+++ AulaCurso/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using Ubiety.Dns.Core;
+
+namespace AulaCurso
+{
+    internal class Program
+    {
+
+        
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua Idade");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            if (idade < 18)
+            {
+
+                Console.BackgroundColor = ConsoleColor.DarkRed; Console.WriteLine("Usuario sem Permissão"); Console.BackgroundColor = ConsoleColor.Black; 
+            }
+
+            if (idade >= 18)
+            {
+
+                Console.BackgroundColor = ConsoleColor.DarkGreen; Console.WriteLine("Permissão concedida"); Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+
+
+
+
+        }
+
+          
+
+    }
+    }
+
